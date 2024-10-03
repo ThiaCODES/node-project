73f01ff1076490a03845d494881bd40f656cca50
ThiaCODES <moshoodfatia@gmail.com>
2024-10-03 17:34:38 +0100

    commit

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 import createError from "http-errors";
 import initDB from "./initDB.js";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
